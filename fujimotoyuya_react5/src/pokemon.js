f05cac125505b01dd8a13b7596c4e92e44dd1eac
Yuya-Fujimoto <web.yf.14@gmail.com>
2024-11-18 17:53:01 +0900

    React-6

---
+++ fujimotoyuya_react5/src/pokemon.js
@@ -0,0 +1,10 @@
+export const getAllPokemon = (url) =>{
+    return new Promise((resolve, reject) =>{
+        fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data)
+            resolve(data);
+        });
+    });
+};
